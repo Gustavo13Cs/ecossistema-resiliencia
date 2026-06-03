bd4a4da700013059de956817626e90c2ddba294b
Gustavo Cunha Santos <105940671+Gustavo13Cs@users.noreply.github.com>
2026-06-03 19:59:57 -0300

    .

--- api/app.controller.ts
+++ api/app.controller.ts
@@ -2,8 +2,14 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
+  
   @Get()
   getHello(): string {
     return 'API do Ecossistema Resiliência está ONLINE e a bombar! 🚀';
+  }
+
+  @Get('ping')
+  getPing(): string {
+    return 'pong';
   }
 }
