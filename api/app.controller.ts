f509d425e601d8b1b70398c4342c5ef2cb0049c2
Gustavo Cunha Santos <105940671+Gustavo13Cs@users.noreply.github.com>
2026-06-03 19:43:35 -0300

    adicionando rota raiz para o UptimeRobot

---
+++ api/app.controller.ts
@@ -0,0 +1,9 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller()
+export class AppController {
+  @Get()
+  getHello(): string {
+    return 'API do Ecossistema Resiliência está ONLINE e a bombar! 🚀';
+  }
+}
